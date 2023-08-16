34dd11e290b140e7c9c3ab6d827cf8f7eda74b46
Ennio <ennioh@icloud.com>
2023-08-16 10:11:50 -0400

    Sobre finalizada

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
-const titulo = document.querySelector('.titulo');
+const frases = ['Desenvolvedor Front-end', 'Front-End Developer'];
+const typewriter = document.querySelector('.titulo');
 
-function typeWriter(elemento) {
-  const textoArray = elemento.innerHTML.split('');
-  elemento.innerHTML = '';
+let indexFraseAtual = 0;
+let indexLetraAtual = 0;
 
-  setInterval(() => {
-    elemento.innerHTML = '';
-    textoArray.forEach((letra, index) => {
-      setTimeout(() => {
-        elemento.innerHTML += letra;
-      }, 100 * index);
-    });
-  }, 3000);
+console.log(frases[indexFraseAtual][0]);
+
+function digitarProximaLetra() {
+  if (indexLetraAtual < frases[indexFraseAtual].length) {
+    typewriter.textContent += frases[indexFraseAtual][indexLetraAtual];
+    indexLetraAtual++;
+    setTimeout(digitarProximaLetra, 50); // Velocidade de digitação
+  } else {
+    setTimeout(apagarTexto, 1500); // Tempo para exibir a frase antes de apagar
+  }
 }
 
-typeWriter(titulo);
+function apagarTexto() {
+  if (typewriter.textContent.length > 0) {
+    typewriter.textContent = typewriter.textContent.slice(0, -1);
+    setTimeout(apagarTexto, 25); // Velocidade de apagar (ajuste conforme desejado)
+  } else {
+    indexFraseAtual = (indexFraseAtual + 1) % frases.length;
+    indexLetraAtual = 0;
+    setTimeout(digitarProximaLetra, 500); // Tempo antes de começar a digitar a próxima frase
+  }
+}
+
+digitarProximaLetra(); // Inicia o efeito de digitação
